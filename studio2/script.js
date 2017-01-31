8838a5e4cb4c2604a872c7077a4b9b4bef3ce279
elaporte <eelaporte@ucdavis.edu>
2017-01-31 09:23:03 -0800

    Add files via upload

--- studio2/script.js
+++ studio2/script.js
@@ -20,12 +20,17 @@ document.addEventListener("DOMContentLoaded", function (event) {
 				    var place1 = document.f.place1.value;
 				    var place2 = document.f.place2.value;
 				    //store myMsg element in variable
+
 				    var myMsg = document.getElementById('myMsg');
-					  myMsg.innerHTML = ('Yall haters <em>' + adj1 + '</em> with that illuminati mess. Paparazzi, catch my ' + noun1 + ', and my cocky fresh. I am so ' + adj2 + ' when I rock my ' + adj3 + ' dress. I am so possessive so I ' + verb1 + ' his Roc necklaces. My daddy ' + place1 + ', Momma ' + place2 + '. You mix that ' + adj4 + ' with that Creole make a Texas bama. I like my baby heir with baby hair and ' + noun2 + '. I ' + verb2 + ' my negro nose with Jackson Five nostrils. Earned all this money but they never take the ' + adj5 + 'out me. I got a hot sauce in my bag, swag');
+					  myMsg.innerHTML = ('Yall haters <em>' + adj1 + '</em> with that illuminati mess. Paparazzi, catch my ' + noun1 + ', and my cocky fresh. I am so ' + adj2 + ' when I rock my ' + adj3 + ' dress. I am so possessive so I ' + verb1 + ' his Roc necklaces. My daddy ' + place1 + ', Momma ' + place2 + '. You mix that ' + adj4 + ' with that Creole make a Texas bama. I like my baby heir with baby hair and ' + noun2 + '. I ' + verb2 + ' my negro nose with Jackson Five nostrils. Earned all this money but they never take the ' + adj5 + ' out me. I got a hot sauce in my bag, swag');
 					//const elementClasses = elementNodeReference.classList;
 					console.log('still working');
 					return false;
 				}
-				//capture the submit event
-				document.f.onsubmit = process();
+
+        //capture the submit event
+        document.f.onsubmit = process;
+        document.f.onreset = function(event) {
+          document.getElementById('myMsg').classList.add("hidden");
+        }
 			});
