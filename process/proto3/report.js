c0d322d1d4914e00d883204a1ebbea1a0d604b62
elaporte <eelaporte@ucdavis.edu>
2017-03-12 15:05:34 -0700

    Add files via upload

--- process/proto3/report.js
+++ process/proto3/report.js
@@ -44,45 +44,80 @@ document.addEventListener("DOMContentLoaded", function () {
         console.log('mousedown on rape');
         rape.style.backgroundColor = '#328c94';
     });
+    rape.addEventListener("dblclick", function() {
+      rape.style.backgroundColor = '#4cd4e1';
+    });
+
     var stalk = document.getElementById('stalk');
     stalk.addEventListener('mousedown', function () {
         console.log('mousedown on stalk');
         stalk.style.backgroundColor = '#328c94';
     });
+    stalk.addEventListener("dblclick", function() {
+      stalk.style.backgroundColor = '#4cd4e1';
+    });
+
     var grope = document.getElementById('grope');
     grope.addEventListener('mousedown', function () {
         console.log('mousedown on grope');
         grope.style.backgroundColor = '#328c94';
     });
+    grope.addEventListener("dblclick", function() {
+      grope.style.backgroundColor = '#4cd4e1';
+    });
+
     var pics = document.getElementById('pics');
     pics.addEventListener('mousedown', function () {
         console.log('mousedown on pics');
         pics.style.backgroundColor = '#328c94';
     });
+    pics.addEventListener("dblclick", function() {
+      pics.style.backgroundColor = '#4cd4e1';
+    });
+
     var exposure = document.getElementById('exposure');
-    exposure.addEventListener('mousedown', function () {
+      exposure.addEventListener('mousedown', function () {
         console.log('mousedown on exposure');
         exposure.style.backgroundColor = '#328c94';
     });
+    exposure.addEventListener("dblclick", function() {
+      exposure.style.backgroundColor = '#4cd4e1';
+    });
+
     var stare = document.getElementById('stare');
     stare.addEventListener('mousedown', function () {
         console.log('mousedown on stare');
         stare.style.backgroundColor = '#328c94';
     });
+    stare.addEventListener("dblclick", function() {
+      stare.style.backgroundColor = '#4cd4e1';
+    });
+
     var invite = document.getElementById('invite');
     invite.addEventListener('mousedown', function () {
         console.log('mousedown on invite');
         invite.style.backgroundColor = '#328c94';
     });
+    invite.addEventListener("dblclick", function() {
+      invite.style.backgroundColor = '#4cd4e1';
+    });
+
     var other = document.getElementById('other');
     other.addEventListener('mousedown', function () {
         console.log('mousedown on other');
         other.style.backgroundColor = '#328c94';
     });
+    other.addEventListener("dblclick", function() {
+      other.style.backgroundColor = '#4cd4e1';
+    });
+
     var catcall = document.getElementById('catcall');
     catcall.addEventListener('mousedown', function () {
         console.log('mousedown on catcall');
         catcall.style.backgroundColor = '#328c94';
+    });
+    catcall.addEventListener("dblclick", function() {
+      catcall.style.backgroundColor = '#4cd4e1';
     });
 
     console.log('did it get here?');
