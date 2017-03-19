ecfcea2ea5857f96a5db0cc518bbafe41a644e56
elaporte <eelaporte@ucdavis.edu>
2017-03-19 00:10:59 -0700

    Firebase start

--- process/proto3/map.js
+++ process/proto3/map.js
@@ -13,41 +13,85 @@ document.addEventListener("DOMContentLoaded", function(event) {
     });
 
     // var reportPage = document.getElementById("reportPage");
-    var submitForm = document.getElementById("submitForm");
     var submitReport = document.getElementById("submitReport");
+    //console.log(submitReport.className);
 
     submitReport.addEventListener("click", function(event) {
         reportPage.style.opacity = 1;
-        mapPage.style.opacity = 1;
+        mapPage.style.opacity = 0.2;
     });
 
-    submitForm.addEventListener("click", function(event) {
-        if ("geolocation" in navigator) {
-            console.log('geolocation is available');
-            navigator.geolocation.getCurrentPosition(success);
-        } else {
-            console.log('geolocation IS NOT available');
-        }
-    });
+    var mainText = document.getElementById("desc");
+    var submitForm = document.getElementById("submitForm");
 
-    function success(position) {
-        console.log('Your current position is:' +
-            '\nlatitude:  ' + position.coords.latitude +
-            '\nlongitude: ' + position.coords.longitude +
-            '\ntimestamp: ' + position.timestamp);
-        // console.log('Your current position is:');
-        // console.log(`Latitude : ${position.coords.latitude}`);
-        // console.log(`Longitude: ${position.coords.longitude}`);
-        // console.log(`More or less ${position.coords.accuracy} meters.`);
+    submitForm.addEventListener("click", submitClick());
 
-        var latitude = position.coords.latitude;
-        var longitude = position.coords.longitude;
-        var timestamp = position.timestamp;
-        alert('Your report has been submitted at the location ' + latitude + ', ' + longitude + ' at time ' + timestamp + '.')
+      function submitClick() {
+      // Get a reference to the database service
+      var firebaseRef = firebase.database().ref();
 
-        return false;
-    };
+      var messageText = mainText.value;
 
+      firebaseRef.push().set(messageText);
+    }
+
+// ---------- spencer's code --------------
+    // submitForm.addEventListener("click", function(event) {
+    //     if ("geolocation" in navigator) {
+    //         console.log('geolocation is available');
+    //         navigator.geolocation.getCurrentPosition(success);
+    //
+    //     } else {
+    //         console.log('geolocation IS NOT available');
+    //     }
+    //     event.preventDefault();
+    // });
+    //
+    // function success(position) {
+    //     console.log('Your current position is:' +
+    //         '\nlatitude:  ' + position.coords.latitude +
+    //         '\nlongitude: ' + position.coords.longitude +
+    //         '\ntimestamp: ' + position.timestamp);
+    //     // console.log('Your current position is:');
+    //     // console.log(`Latitude : ${position.coords.latitude}`);
+    //     // console.log(`Longitude: ${position.coords.longitude}`);
+    //     // console.log(`More or less ${position.coords.accuracy} meters.`);
+    //
+    //     var latitude = position.coords.latitude;
+    //     var longitude = position.coords.longitude;
+    //     var timestamp = position.timestamp;
+    //
+    //     alert('Your report has been submitted at the location ' + latitude + ', ' + longitude + ' at time ' + timestamp + '.')};
+
+
+
+        // function writeUserData(lat, lon, time) {
+        //   var latitude = position.coords.latitude;
+        //   var longitude = position.coords.longitude;
+        //   var timestamp = position.timestamp;
+        //     firebase.database().ref('report').set({
+        //         lat: latitude,
+        //         lon: longitude,
+        //         time: timestamp
+        //     });
+        // }
+        // var userReportRef = firebase.database().ref('report/');
+        // userReportRef.on('value', function(snapshot) {
+        //     updateUserReport(postElement, snapshot.val());
+        // });
+        //
+        // var newRef = {
+        //   lat: latitude,
+        //   lon: longitude,
+        //   time: timestamp
+        // };
+        //
+        // var newPostKey = firebase.database().ref.child('report').push().key;
+        // // var newRef = firebase.database.ref).child('report').push();
+        // var updates = {};
+        // updates['/report/' + newPostKey] = newRef;
+        //
+        // return firebase.database().ref().updates(updates);
     // window.onload = function () { initialize() };
 
     // mapPage.style.display = "none";
@@ -160,3 +204,13 @@ document.addEventListener("DOMContentLoaded", function(event) {
     // });
 
 });
+
+function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 13,
+        center: {
+            lat: 38.5382363,
+            lng: -121.7476284
+        }
+    });
+}
