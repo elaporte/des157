a0d65e669ab0d1178171028667a77954977cb671
elaporte <eelaporte@ucdavis.edu>
2017-03-20 11:11:01 -0700

    Final folder updated

--- process/final/script.js
+++ process/final/script.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
     console.log("DOM fully loaded and parsed");
     console.log('Icons made by http://www.freepik.com. Freepik from www.flaticon.com is licensed by http://creativecommons.org/licenses/by/3.0/ Creative Commons BY 3.0 CC 3.0 BY');
 
+    //var firebase = new Firebase("https://out-of-the-shadows.firebaseio.com");
+
     var go = document.getElementById("go");
     var loadingPage = document.getElementById("loadingPage");
     var mapPage = document.getElementById("mapPage");
@@ -13,219 +15,191 @@ document.addEventListener("DOMContentLoaded", function(event) {
     });
     console.log("checkpoint 1");
 
-    // var reportPage = document.getElementById("reportPage");
     var submitReport = document.getElementById("submitReport");
-    //console.log(submitReport.className);
 
     submitReport.addEventListener("click", function(event) {
-        reportPage.style.opacity = 1;
+        reportPage.style.display = 'block';
         mapPage.style.opacity = 0.2;
     });
-      console.log("checkpoint 2");
-    var mainText = document.getElementById("desc");
+    console.log("checkpoint 2");
+
     var submitForm = document.getElementById("submitForm");
 
-    //submitForm.addEventListener("click", submitClick());
+    submitForm.addEventListener("click", function(event) {
+        window.alert("Thank you for sharing your story");
+        reportPage.style.display = "none";
+        mapPage.style.opacity = 1;
+    });
 
     submitForm.addEventListener("click", function(event) {
-      reportPage.style.opacity = 0;
-      mapPage.style.opacity = 1;
+        if ("geolocation" in navigator) {
+            console.log('geolocation is available');
+            navigator.geolocation.getCurrentPosition(success);
+
+        } else {
+            console.log('geolocation IS NOT available');
+        }
+        event.preventDefault();
     });
 
-  console.log("checkpoint 3");
+    //-------- 12 hour timer --------
+    var date_format = '12'; /* FORMAT CAN BE 12 hour (12) OR 24 hour (24)*/
+    var d = new Date();
+    var hour = d.getHours(); /* Returns the hour (from 0-23) */
+    var minutes = d.getMinutes(); /* Returns the minutes (from 0-59) */
+    var result = hour;
+    var ext = '';
 
-  function submitClick() {
+    if (date_format == '12') {
+        if (hour > 12) {
+            ext = 'PM';
+            hour = (hour - 12);
 
-        window.alert("submit button check");
-      // Get a reference to the database service
-      var firebaseRef = firebase.database().ref();
+            if (hour < 10) {
+                result = "0" + hour;
+            } else if (hour == 12) {
+                hour = "00";
+                ext = 'AM';
+            }
+        } else if (hour < 12) {
+            result = ((hour < 10) ? "0" + hour : hour);
+            ext = 'AM';
+        } else if (hour == 12) {
+            ext = 'PM';
+        }
+    }
 
-      var messageText = mainText.value;
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
 
-      firebaseRef.child("text").set("another value");
-      console.log("checkpoint 3.5");
-      return false;
-     }
+    result = result + ":" + minutes + ' ' + ext;
+    // ---------- End 12 hour time function
 
-// ---------- spencer's code --------------
-    // submitForm.addEventListener("click", function(event) {
-    //     if ("geolocation" in navigator) {
-    //         console.log('geolocation is available');
-    //         navigator.geolocation.getCurrentPosition(success);
-    //
-    //     } else {
-    //         console.log('geolocation IS NOT available');
-    //     }
-    //     event.preventDefault();
-    // });
-    //
-    // function success(position) {
-    //     console.log('Your current position is:' +
-    //         '\nlatitude:  ' + position.coords.latitude +
-    //         '\nlongitude: ' + position.coords.longitude +
-    //         '\ntimestamp: ' + position.timestamp);
-    //     // console.log('Your current position is:');
-    //     // console.log(`Latitude : ${position.coords.latitude}`);
-    //     // console.log(`Longitude: ${position.coords.longitude}`);
-    //     // console.log(`More or less ${position.coords.accuracy} meters.`);
-    //
-    //     var latitude = position.coords.latitude;
-    //     var longitude = position.coords.longitude;
-    //     var timestamp = position.timestamp;
-    //
-    //     alert('Your report has been submitted at the location ' + latitude + ', ' + longitude + ' at time ' + timestamp + '.')};
-
-
-
-        // function writeUserData(lat, lon, time) {
-        //   var latitude = position.coords.latitude;
-        //   var longitude = position.coords.longitude;
-        //   var timestamp = position.timestamp;
-        //     firebase.database().ref('report').set({
-        //         lat: latitude,
-        //         lon: longitude,
-        //         time: timestamp
-        //     });
-        // }
-        // var userReportRef = firebase.database().ref('report/');
-        // userReportRef.on('value', function(snapshot) {
-        //     updateUserReport(postElement, snapshot.val());
-        // });
-        //
-        // var newRef = {
-        //   lat: latitude,
-        //   lon: longitude,
-        //   time: timestamp
-        // };
-        //
-        // var newPostKey = firebase.database().ref.child('report').push().key;
-        // // var newRef = firebase.database.ref).child('report').push();
-        // var updates = {};
-        // updates['/report/' + newPostKey] = newRef;
-        //
-        // return firebase.database().ref().updates(updates);
-    // window.onload = function () { initialize() };
-
-    // mapPage.style.display = "none";
-    // reportPage.style.display = "block";
-    // submit.addEventListener("click", function(event) {
-    //     reportPage.style.display = "none";
-    //     mapPage.style.display = "block";
-    // });
-
-
-    // document.f.onsubmit = processForm;
-    // // console.log('and here?')
-    //
-    // function processForm() {
-
-    // //     console.log('how about now?');
-    // //     //store user name in a variable
-    // //     var time = document.f.userTime.value;
-    // //     console.log('and now?');
-    // //     var date = document.f.userDate.value;
-    // //     console.log('maybe now?');
-    // //     var location = document.f.userLocation.value;
-    // //     console.log('cmon now?');
-    // //     alert('Report submitted for '  date  ' at '  time  ' at '  location  '. Thank you for sharing your story.');
-    // //
-    // //     console.log('why yes it did');
-    // //
-    // //     return false;
-    // // };
-    //
     console.log("checkpoint 4");
-    var catcall = document.getElementById('catcall');
-    catcall.addEventListener('mousedown', function() {
-        console.log('mousedown on catcall');
-        catcall.style.backgroundColor = '#328c94';
-    });
-    catcall.addEventListener("dblclick", function() {
-        catcall.style.backgroundColor = '#4cd4e1';
+
+    // });
+    var catcall = document.getElementById('catcall').addEventListener("click", catcall);
+    var rape = document.getElementById('rape').addEventListener("click", rape);
+    var stalk = document.getElementById('stalk').addEventListener("click", stalk);
+    var grope = document.getElementById('grope').addEventListener("click", grope);
+    var pics = document.getElementById('pics').addEventListener("click", pics);
+    var exposure = document.getElementById('exposure').addEventListener("click", exposure);
+    var stare = document.getElementById('stare').addEventListener("click", stare);
+    var invite = document.getElementById('invite').addEventListener("click", invite);
+
+    var categorySelected = '';
+
+    $(document).ready(function() {
+        $("[class=button2]").css("background-color", "#4cd4e1");
+        categorySelected = 'none';
     });
 
-    var rape = document.getElementById('rape');
-    rape.addEventListener('mousedown', function() {
-        console.log('mousedown on rape');
-        rape.style.backgroundColor = '#328c94';
-    });
-    rape.addEventListener("dblclick", function() {
-        rape.style.backgroundColor = '#4cd4e1';
-    });
+    function catcall() {
+        $("[id=catcall]").css("background-color", "#328c94");
+        categorySelected = 'Catcalling';
+    }
 
-    var stalk = document.getElementById('stalk');
-    stalk.addEventListener('mousedown', function() {
-        console.log('mousedown on stalk');
-        stalk.style.backgroundColor = '#328c94';
-    });
-    stalk.addEventListener("dblclick", function() {
-        stalk.style.backgroundColor = '#4cd4e1';
-    });
+    function rape() {
+        $("[id=rape]").css("background-color", "#328c94");
+        categorySelected = 'Rape/Sexual Assault';
+    }
 
-    var grope = document.getElementById('grope');
-    grope.addEventListener('mousedown', function() {
-        console.log('mousedown on grope');
-        grope.style.backgroundColor = '#328c94';
-    });
-    grope.addEventListener("dblclick", function() {
-        grope.style.backgroundColor = '#4cd4e1';
-    });
+    function stalk() {
+        $("[id=stalk]").css("background-color", "#328c94");
+        categorySelected = 'Stalking';
+    }
 
-    var pics = document.getElementById('pics');
-    pics.addEventListener('mousedown', function() {
-        console.log('mousedown on pics');
-        pics.style.backgroundColor = '#328c94';
-    });
-    pics.addEventListener("dblclick", function() {
-        pics.style.backgroundColor = '#4cd4e1';
-    });
+    function grope() {
+        $("[id=grope]").css("background-color", "#328c94");
+        categorySelected = 'Groping/touching';
+    }
 
-    var exposure = document.getElementById('exposure');
-    exposure.addEventListener('mousedown', function() {
-        console.log('mousedown on exposure');
-        exposure.style.backgroundColor = '#328c94';
-    });
-    exposure.addEventListener("dblclick", function() {
-        exposure.style.backgroundColor = '#4cd4e1';
-    });
+    function pics() {
+        $("[id=pics]").css("background-color", "#328c94");
+        categorySelected = 'Taking pictures';
+    }
 
-    var stare = document.getElementById('stare');
-    stare.addEventListener('mousedown', function() {
-        console.log('mousedown on stare');
-        stare.style.backgroundColor = '#328c94';
-    });
-    stare.addEventListener("dblclick", function() {
-        stare.style.backgroundColor = '#4cd4e1';
-    });
+    function exposure() {
+        $("[id=exposure]").css("background-color", "#328c94");
+        categorySelected = 'Indecent exposure';
+    }
 
-    var invite = document.getElementById('invite');
-    invite.addEventListener('mousedown', function() {
-        console.log('mousedown on invite');
-        invite.style.backgroundColor = '#328c94';
-    });
-    invite.addEventListener("dblclick", function() {
-        invite.style.backgroundColor = '#4cd4e1';
-    });
+    function stare() {
+        $("[id=stare]").css("background-color", "#328c94");
+        categorySelected = 'Staring';
+    }
+
+    function invite() {
+        $("[id=invite]").css("background-color", "#328c94");
+        categorySelected = 'Sexual invites';
+    }
+
     console.log("checkpoint 5");
 
-    // var other = document.getElementById('other');
-    // other.addEventListener('mousedown', function () {
-    //     console.log('mousedown on other');
-    //     other.style.backgroundColor = '#328c94';
-    // });
-    // other.addEventListener("dblclick", function() {
-    //   other.style.backgroundColor = '#4cd4e1';
-    // });
+    function success(position) {
+        console.log('Your current position is:' +
+            '\nlatitude:  ' + position.coords.latitude +
+            '\nlongitude: ' + position.coords.longitude +
+            '\ntimestamp: ' + position.timestamp +
+            '\ncategory:  ' + categorySelected);
+
+        var latitude = position.coords.latitude;
+        var longitude = position.coords.longitude;
+        var timestamp = position.timestamp;
+
+        // Get a reference to the database service
+        var firebaseRef = firebase.database().ref();
+        var mainText = document.getElementById("desc");
+        var messageText = mainText.value;
+
+        var data = {
+            description: messageText,
+            timestamp: timestamp,
+            lat: latitude,
+            lng: longitude,
+            category: categorySelected
+        };
+
+        var myLatlng = new google.maps.LatLng(latitude, longitude);
+        var mapOptions = {
+            zoom: 16,
+            center: myLatlng
+        };
+        var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+        var icon = 'http://maps.google.com/mapfiles/kml/pal3/icon49.png';
+
+        var marker = new google.maps.Marker({
+            position: myLatlng,
+            icon: icon,
+        });
+
+        var contentString = "A report was submitted here at " + result +
+            "\nDescription: " + messageText +
+            "\nCategory: " + categorySelected;
+
+        var infowindow = new google.maps.InfoWindow({
+            content: contentString,
+            maxWidth: 200
+        });
+
+        marker.addListener('click', function() {
+            infowindow.open(map, marker);
+        });
+
+        // To add the marker to the map, call setMap();
+        marker.setMap(map);
+
+        firebaseRef.child('events').push().set(data);
+        console.log("checkpoint 3");
+        console.log(messageText, latitude, longitude, timestamp, categorySelected);
+
+        //------------------- TEST ----------------//
+        return false;
+
+    }
+    // var category = document.getElementsByClassName('button2');
+    // category.addEventListener("click", clicked);
 
 });
-
-function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 13,
-        center: {
-            lat: 38.5382363,
-            lng: -121.7476284
-        }
-    });
-  console.log("checkpoint 6");
-}
